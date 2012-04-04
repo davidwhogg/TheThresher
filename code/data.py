1131ff8f7f6c66e47832cdac4907e98d803c2f66
Dan F-M <danfm@nyu.edu>
2012-04-04 18:05:07 +0000

    lazy loading

--- code/data.py
+++ code/data.py
@@ -11,9 +11,6 @@ import os
 
 import pyfits
 
-_bp = os.environ.get("LUCKY_DATA",
-                "/data2/dfm/mars/bpl1m001-en07-20120304/unspooled")
-
 class Image(object):
     """
     A simple wrapper around the FITS file for the Mars lucky imaging dataset.
@@ -23,18 +20,37 @@ class Image(object):
     * `_id` (int): The
 
     """
+
+    _bp = os.environ.get("LUCKY_DATA",
+                "/data2/dfm/mars/bpl1m001-en07-20120304/unspooled")
+    _fn_format = "bpl1m001-en07-20120304-{0}-e00.fits"
+
     def __init__(self, _id):
-        fn = "bpl1m001-en07-20120304-%04d-e00.fits"%_id
-        full_path = os.path.join(_bp, fn)
-        f = pyfits.open(full_path)
+        self.fn = self.fn_format.format("%04d"%_id)
+        self.path = os.path.join(self._bp, self.fn)
+        self._image = None
+
+    @classmethod
+    def get_all(cls):
+
+
+    @property
+    def image(self):
+        """Lazily load the image file only when needed."""
+        if self._image is not None:
+            return self._image
+
+        f = pyfits.open(self.path)
 
         # Grab the image and header from the FITS file.
-        self.image = f[0].data
+        self._image = f[0].data
         self.info = {}
         for k in f[0].header.keys():
             self.info[k] = f[0].header[k]
 
         f.close()
+
+        return self._image
 
     def __getitem__(self, s):
         return self.image[s]
