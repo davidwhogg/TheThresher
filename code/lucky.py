4cedc915966ef949db18689fafb44a806ec0a839
Dan F-M <danfm@nyu.edu>
2012-05-04 13:47:17 -0400

    starting huge refactor

--- code/lucky.py
+++ code/lucky.py
@@ -26,14 +26,9 @@ notes:
 
 """
 
-if __name__ == "__main__":
-    import matplotlib
-    matplotlib.use('Agg')
-    from matplotlib import rc
-    rc('font', family="serif", serif="Computer Modern Roman", size=12)
-    rc('text', usetex=True)
+__all__ = ["infer_psf", "inference_step", "save_scene", "read_scene"]
 
-import matplotlib.pyplot as plt
+
 import numpy as np
 from scipy.sparse import csr_matrix
 from scipy.sparse.linalg import lsqr
@@ -257,81 +252,12 @@ def inference_step(data, oldScene, alpha, psfL2norm, sceneL2norm, nonNegative,
         print 'inference_step(): clipped scene to non-negative'
     print 'inference_step(): new scene:', np.min(newScene), \
             np.median(newScene), np.max(newScene)
-    if plot is not None:
-        plot_inference_step(data, thisScene, newPsf, newScene, plot)
-    if splot is not None:
-        plot_inference_step(data, thisScene, newPsf, newScene, splot,
-                stretch=3.)
+    # if plot is not None:
+    #     plot_inference_step(data, thisScene, newPsf, newScene, plot)
+    # if splot is not None:
+    #     plot_inference_step(data, thisScene, newPsf, newScene, splot,
+    #             stretch=3.)
     return newPsf, newScene
-
-
-def plot_inference_step(data, thisScene, thisPsf, newScene, filename,
-        stretch=None):
-    '''
-    # `plot_inference_step()`:
-
-    Make plots for `inference_step()`.
-    '''
-    fig = plt.figure(figsize=(12, 8))
-    plt.clf()
-    ax1 = fig.add_subplot(231)
-
-    ax2 = fig.add_subplot(232)
-    ax3 = fig.add_subplot(233)
-    ax4 = fig.add_subplot(234)
-    ax5 = fig.add_subplot(235)
-    ax6 = fig.add_subplot(236)
-
-    def hogg_plot_image_histeq(ax, im):
-        shape = im.shape
-        size = im.size
-        foo = np.zeros(size)
-        foo[np.argsort(im.reshape(size))] = np.arange(size)
-        return ax.imshow(foo.reshape(shape), cmap="gray",
-                interpolation="nearest")
-
-    def hogg_plot_image(ax, im, stretch):
-        if stretch is None:
-            a = np.median(im)
-            b = np.max(im)
-            vmin = a - b
-            vmax = a + b
-        else:
-            a = np.median(im)
-            b = np.sort(im.reshape(im.size))[0.95 * im.size]
-            vmin = a - 3. * b / stretch
-            vmax = a + 3. * b / stretch
-        return ax.imshow(im, cmap="gray", interpolation="nearest", vmin=vmin,
-                vmax=vmax)
-
-    hw1 = (thisPsf.shape[0] - 1) / 2
-    hw2 = (data.shape[0] - thisPsf.shape[0] - 1) / 2
-    hogg_plot_image(ax1, data, stretch)
-    ax1.set_title("data")
-    hogg_plot_image(ax2, convolve(scene, thisPsf, mode="valid"), stretch)
-    ax2.set_title(r"[inferred PSF] $\ast$ [previous scene]")
-    hogg_plot_image(ax3, convolve(newScene, thisPsf, mode="valid"), stretch)
-    ax3.set_title(r"[inferred PSF] $\ast$ [inferred scene]")
-    bigPsf = np.zeros_like(data)
-    bigPsf[hw2:hw2 + thisPsf.shape[0], hw2:hw2 + thisPsf.shape[1]] = thisPsf
-    hogg_plot_image(ax4, bigPsf, stretch)
-    ax4.set_title("inferred PSF (deconvolved)")
-    hogg_plot_image(ax5, thisScene[hw1:-hw1, hw1:-hw1], stretch)
-    ax5.set_title("inferred scene (cropped)")
-    hogg_plot_image(ax6, newScene[hw1:-hw1, hw1:-hw1], stretch)
-    ax6.set_title("updated scene (cropped)")
-    hogg_savefig(filename)
-    return None
-
-
-def hogg_savefig(fn):
-    '''
-    # `hogg_savefig()`:
-
-    Hogg likes a verbose `savefig()`!
-    '''
-    print "hogg_savefig(): writing %s" % fn
-    return plt.savefig(fn)
 
 
 def save_scene(image, fn, clobber=True):
