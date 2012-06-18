617ee7ab6ef55c6cea55b32069a207ac1ef90af8
Dan F-M <danfm@nyu.edu>
2012-06-18 11:22:19 -0400

    ints don't have lengths dummy

--- thresher/thresher.py
+++ thresher/thresher.py
@@ -219,7 +219,7 @@ class Scene(object):
 
         # Run lucky imaging. MAGIC: co-add the top 1 percent.
         images, ranks, scene = self.run_lucky(top_percent=1)
-        scene = scene[0]
+        scene = scene[1]
         self.scene = scene - np.median(scene)
 
         # HACK part 2.
@@ -273,6 +273,7 @@ class Scene(object):
         elif top_percent is not None:
             top = max(1, int(top_percent * 0.01 * len(ranked)))
 
+        top = np.atleast_1d(top)
         final = np.zeros((len(top) + 1, self.size, self.size))
         for j, t in enumerate(np.atleast_1d(top)):
             for i, k in enumerate(fns[:t]):
