d3c8d0656b5238f959279bf1e17b188885d02cd2
Dan F-M <danfm@nyu.edu>
2012-07-03 12:53:41 +0200

    PSF error and PEP 8 for make_fake

--- thresher/make_fake.py
+++ thresher/make_fake.py
@@ -5,6 +5,7 @@ This file is part of The Thresher.
 import numpy as np
 import pyfits as pyf
 
+
 def make_image(shape, positions, fluxes, sky, sky_sigma, psf_sigma):
     """
     Make one image.
@@ -15,9 +16,11 @@ def make_image(shape, positions, fluxes, sky, sky_sigma, psf_sigma):
     nx, ny = shape
     xg, yg = np.meshgrid(range(nx), range(ny))
     for pos, f in zip(positions, fluxes):
-        kernel = np.exp(-0.5 * ((xg - pos[0]) ** 2 + (yg - pos[1]) ** 2))
+        kernel = np.exp(-0.5 * ((xg - pos[0]) ** 2 + (yg - pos[1]) ** 2)
+                / psf_sigma ** 2)
         img += f * kernel / (2. * np.pi * psf_sigma ** 2)
     return img
+
 
 def make_constant_psf_imaging_run(N, prefix):
     """
@@ -29,12 +32,12 @@ def make_constant_psf_imaging_run(N, prefix):
     positions = np.array([[13.1, 45.5],
                           [65.3, 61.2],
                           [51.5, 73.7]])
-    fluxes = np.array([10., 100., 50.])
+    fluxes = 2 * np.array([10., 100., 50.])
     sky_sigma = 1.0
-    psf_sigma = 1.5
+    psf_sigma = 1.8
     for n in range(N):
         sky = 10. * np.random.uniform()
-        offset = 20. * np.random.normal(size=(1,2))
+        offset = 20. * np.random.normal(size=(1, 2))
         ps = positions + offset
         fn = "%s_%05d.fits" % (prefix, n)
         img = make_image(shape, ps, fluxes, sky, sky_sigma, psf_sigma)
